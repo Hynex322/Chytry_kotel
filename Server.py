e242763208b8accbe8792958b63b6fdb0064d41e
Hynex322 <hynek.telcer@hotmail.com>
2025-01-05 00:17:10 +0100

    test3

--- Server.py
+++ Server.py
@@ -21,7 +21,7 @@ def temp():
 @app.route("/maxTemp")   
 def maxTemp():
     global max_Temp
-    return str(max_Temp[0])
+    return str(max_Temp)
     
 @app.route("/reboot")
 def reboot_server():
