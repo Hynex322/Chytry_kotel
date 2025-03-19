a4fcd8291b1eb11ba448b2d0c8e3b597707015fe
Hynex322 <hynek.telcer@hotmail.com>
2025-03-19 14:38:57 +0100

    SOS
    SOS
    
    SOS
    
    SOS
    
    SoS
    
    SoS
    
    SoS
    
    SOS
    
    SoS
    
    SOS
    
    SOS
    
    SOS
    
    SOS
    
    SOS
    
    SOS

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from pickle import FALSE
 import time
-import schedule
+import schedule, sched
 from Temperature import TemperatureSensor
 from Relay import Relay
 import os
@@ -21,6 +21,8 @@ check_delay = 30  # delay between temp checks
 siren_btn_shutoff = 600  # shutoff time after btn press
 temp_bounds = [20, 120]  # bounds for LEDs
 temp_warn = 85  # limit temp for warning
+TEMP_SOS_WARN = 80 # limit temp for SOS warning
+SOS_OMEZENI_SPUSTENI = 300 # omezeni kolikrat za minutu se SOS spusti ve vterinach
 press_for_turnoff = 50  # times btn is pressed to turnoff
 lowBound  = 40
 FALLaLARM = 2
@@ -28,6 +30,7 @@ HISTORYlEN = 10
 PERIODhISTORY = 120 # ve vterinach, vzdalenost zaznamu 
 TEPLOTA_ROZTOPENO = 50 #°C #Hranice od kdy se aktivuje automatickemu vypnuti
 LIMIT_VYPNUTI = 30     #°C #Hranice automatickeho vypnuti
+SPODNI_ALARM_LIMIT= 40 #°C #Spodni hranice od kdy prestane hlasit FALLaLARM
 
 main_tmp_sensor = '28-00000a10a10f'
 
@@ -57,7 +60,7 @@ manager = Manager()
 maxTemp = manager.Value('d', 0)  # 'd' znamená double (desetinné číslo)
 averageTemp = manager.Value('d', 0)
 history = manager.list()
-
+sos_omezeni_counter=False
 # turn off the relay
 Pin(26).low()
 
@@ -88,6 +91,57 @@ def Decline_alert():
     time.sleep(0.3)
     sirena.low() #siren_relay.off()
 
+def alert_long():
+    sirena.high() #siren_relay.on()
+    time.sleep(0.5)
+    sirena.low() #siren_relay.off()
+
+def alert_short():
+    sirena.high() #siren_relay.on()
+    time.sleep(0.2)
+    sirena.low() #siren_relay.off()
+
+def Sos():
+    if Sos_omezeni():
+        alert_short()
+        time.sleep(0.1)
+        alert_short()
+        time.sleep(0.1)
+        alert_short()
+        time.sleep(0.1)
+        alert_long()
+        time.sleep(0.1)
+        alert_long()
+        time.sleep(0.1)
+        alert_long()
+        time.sleep(0.1)
+        alert_short()
+        time.sleep(0.1)
+        alert_short()
+        time.sleep(0.1)
+        alert_short()
+
+# Vytvoření plánovače
+scheduler = sched.scheduler(time.time, time.sleep)
+
+def Sos_zruseni_omezeni():
+    global sos_omezeni_counter
+    sos_omezeni_counter=False
+
+def Sos_omezeni():
+    global sos_omezeni_counter
+    if sos_omezeni_counter:
+        return False
+    else:
+        sos_omezeni_counter = True
+        scheduler.enter(SOS_OMEZENI_SPUSTENI, 1, Sos_zruseni_omezeni)
+        return True
+    
+def run_scheduler():
+    while True:
+        scheduler.run(blocking=True)
+        time.sleep(1)  # Krátká pauza pro uvolnění CPU
+
 def PrumerTeploty():
     global averageTemp, history_60
 
@@ -100,10 +154,10 @@ def PrumerTeploty():
     if len(history_60) > 0:
         averageTemp.value = sum(history_60) / len(history_60)
     print("hodnota averageTemp: ", averageTemp )
-    
+
 #definice jak casto se spusti ulozeni prumerne teploty
 schedule.every().minute.do(PrumerTeploty)
-       
+
 # program loop
 def main():
     global siren_ontime, maxTemp, history
@@ -112,17 +166,18 @@ def main():
     blik_i = 0
     odpocet = 0
     roztopen_kotel = 0
+   
     while True:
         temperature = tmp_sensor.get_temperature()
         #print("[teplota]", temperature)
-
         schedule.run_pending()  # Zkontroluje, zda je čas na spuštění úlohy
 
         if temperature >= temp_warn:
             Start_alarm()
         else:
             Stop_alarm()
-
+        if temperature >= TEMP_SOS_WARN:
+            Sos()              
         if temperature < lowBound:
             yellowLed.high()
         else:
@@ -137,7 +192,7 @@ def main():
         if posledni_maxTemp < temperature:
             posledni_maxTemp = temperature
             #print("Nová posledni maximální hodnota: ", posledni_maxTemp)
-        elif temperature < (posledni_maxTemp - FALLaLARM):
+        elif ((temperature < (posledni_maxTemp - FALLaLARM)) and (temperature > SPODNI_ALARM_LIMIT)):
             Decline_alert()
             posledni_maxTemp = temperature
             #print("Maximální teplota byla snížena na: ", posledni_maxTemp)
@@ -184,17 +239,6 @@ def main():
                 else:
                     print("kotel se vypne ",ii)
             if ii == 5:
-                sirena.high() #siren_relay.on()
-                time.sleep(0.1)
-                sirena.low() #siren_relay.off()
-                time.sleep(0.1)
-                sirena.high() #siren_relay.on()
-                time.sleep(0.1)
-                sirena.low() #siren_relay.off()
-                time.sleep(0.1)
-                sirena.high() #siren_relay.on()
-                time.sleep(0.1)
-                sirena.low() #siren_relay.off()
                 print("vypinam kotel")
                 time.sleep(120.0) #zpozdeni kdyby se nekde stala chyba
                 ShutDown()
@@ -214,5 +258,8 @@ if __name__ == '__main__':
     print("Konec testu sirény")
     Server.run_async(tmp_sensor, ip, history, maxTemp, averageTemp)
     # Server.run_remote(tmp_sensor, url=remote_url, key=remote_key)
+    # Spustíme plánovač v samostatném vlákně
+    scheduler_thread = threading.Thread(target=run_scheduler, daemon=True)
+    scheduler_thread.start()
     main()
 
